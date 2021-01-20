85e980a1764aa90d3d2a7aa06239c5370cc4d9fb
thobauma <thobauma@ethz.ch>
2021-01-20 14:34:49 +0100

    stest,
    missing arguments N

--- tests/gpu_multi/stest.py
+++ tests/gpu_multi/stest.py
@@ -18,11 +18,22 @@ def axpy(A, X, Y):
 
         out = in_A * in_X + in_Y
 
-@dace.program(dace.float64, dace.float64, dace.float64, dace.float64, dace.float64[N], dace.float64[N], dace.float64[N], dace.float64[N], dace.float64[N])
-def compound(a,b,c,alpha,A,B,C,X,Z):
+@dace.program(dace.float64, dace.float64[N], dace.float64[N])
+def axpy2(A, X, Y):
+    @dace.map(_[0:N])
+    def multiplication(j):
+        in_A << A
+        in_X << X[j]
+        in_Y << Y[j]
+        out >> Y[j]
+
+        out = in_A * in_X + in_Y
+
+@dace.program(dace.float64, dace.float64, dace.float64, dace.float64, dace.float64[N], dace.float64[N], dace.float64[N], dace.float64[N], dace.float64[N], dace.float64[N])
+def compound(a,b,c,alpha,A,B,C,D,X,Z):
     axpy(a,A,X)
     axpy(c,A,Z)
-    # axpy(b,B,A)
+    axpy2(b,B,D)
     # axpy(b,A,X)
     axpy(alpha,C,X)
     # axpy(alpha,B,X)
@@ -32,19 +43,19 @@ def find_map_by_param(sdfg: dace.SDFG, pname: str) -> dace.nodes.MapEntry:
     return next(n for n, _ in sdfg.all_nodes_recursive()
                 if isinstance(n, dace.nodes.MapEntry) and pname in n.params)
 
-
 def add_gpu_location(sdfg: dace.SDFG, mapEntry, gpu):
+    mapEntry.location['gpu']=gpu
     graph = sdfg.nodes()[sdfg.sdfg_id]
     exit_edges = [
             e for e in graph.out_edges(mapEntry)
-            if isinstance(e.dst, nodes.Tasklet)
+            if isinstance(e.dst, dace.nodes.Tasklet)
         ]
     for e in exit_edges:
         tasklet = e.dst
         tasklet.location = {'gpu': gpu}
     entry_edges = [
             e for e in graph.in_edges(mapEntry)
-            if isinstance(e.src, nodes.AccessNode)
+            if isinstance(e.src, dace.nodes.AccessNode)
         ]
     for e in entry_edges:
         data_node = e.src
@@ -53,24 +64,40 @@ def add_gpu_location(sdfg: dace.SDFG, mapEntry, gpu):
 # @pytest.mark.gpu
 def test_compound():
     size = 256
-
+    N.set(size)
     np.random.seed(0)
-    A = np.random.rand()
-    X = np.random.rand(size)
-    Y = np.random.rand(size)
-    Z = np.copy(Y)
+    a = np.random.rand()
+    b = np.random.rand()
+    c = np.random.rand()
+    alpha = np.random.rand()
+    A = np.random.rand(N.get()).astype(np.float64)
+    B = np.random.rand(N.get()).astype(np.float64)
+    C = np.random.rand(N.get()).astype(np.float64)
+    D = np.random.rand(N.get()).astype(np.float64)
+    X = np.random.rand(N.get()).astype(np.float64)
+    Z = np.random.rand(N.get()).astype(np.float64)
+    Zcopy = np.copy(Z)
+    Xcopy = np.copy(X)
+    Dcopy = np.copy(D)
 
     sdfg: dace.SDFG = compound.to_sdfg()
     sdfg.apply_strict_transformations()
     sdfg.apply_transformations(GPUTransformSDFG)
+    
+    map_entry = find_map_by_param(sdfg, 'j')
 
+    add_gpu_location(sdfg, map_entry, 1)
 
-    sdfg.view()
-    sdfg.compile()
-    # sdfg(A=A, X=X, Y=Y, N=size)
+    # sdfg.view()
+    # sdfg.compile()
+    sdfg(a=a,b=b,c=c,alpha=alpha,A=A,B=B,C=C,D=D,X=X,Z=Z)
 
-    # assert np.allclose(Y, A * X + Z)
-    # print('PASS')
+    assert np.allclose(D, b * B + Dcopy)
+    print('PASS 1/3')
+    assert np.allclose(Z, c * A + Zcopy)
+    print('PASS 2/3')
+    assert np.allclose(X, alpha * C+(a*A+Xcopy))
+    print('PASS 3/3')
 
 
 if __name__ == "__main__":
